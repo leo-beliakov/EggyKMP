8718052f1ff5c59270db643af155d4a7c9597057
Leo Belyakov <lbelyakov@cochlear.com>
2024-11-14 18:04:23 +1100

    Added Root Screen. Added Navigaion.

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -7,6 +7,7 @@ plugins {
     alias(libs.plugins.androidApplication)
     alias(libs.plugins.composeMultiplatform)
     alias(libs.plugins.composeCompiler)
+    alias(libs.plugins.kotlinSerialization)
 }
 
 kotlin {
@@ -40,10 +41,12 @@ kotlin {
             implementation(compose.ui)
             implementation(compose.components.resources)
             implementation(compose.components.uiToolingPreview)
+            implementation(libs.kotlin.serialization)
             implementation(libs.androidx.lifecycle.viewmodel)
             implementation(libs.androidx.lifecycle.runtime.compose)
             implementation(libs.compose.constraint.layout)
             implementation(libs.compose.material3)
+            implementation(libs.navigation.compose)
         }
     }
 }
--- composeApp/src/commonMain/kotlin/com/leoapps/eggy/App.kt
+++ composeApp/src/commonMain/kotlin/com/leoapps/eggy/App.kt
@@ -2,6 +2,7 @@ package com.leoapps.eggy
 
 import androidx.compose.runtime.Composable
 import com.leoapps.eggy.base.ui.theme.EggyTheme
+import com.leoapps.eggy.root.presentation.RootScreen
 import com.leoapps.eggy.welcome.presentation.BoilSetupScreen
 import com.leoapps.eggy.welcome.presentation.WelcomeScreen
 import com.leoapps.setup.domain.CalculateBoilingTimeUseCase
@@ -12,9 +13,6 @@ import org.jetbrains.compose.ui.tooling.preview.Preview
 @Preview
 fun App() {
     EggyTheme {
-        BoilSetupScreen(
-            viewModel = BoilSetupViewModel(CalculateBoilingTimeUseCase()),
-            onContinueClicked = { c, r -> }
-        )
+        RootScreen()
     }
 }
