91ff720f4868f27e4db0ee664f4b89504341c030
Leo Belyakov <lbelyakov@cochlear.com>
2024-12-03 16:45:10 +1100

    Added Vibration on Timer Completion in IOS

--- composeApp/src/iosMain/kotlin/com/leoapps/eggy/common/vibrator/VibrationManagerIosImpl.kt
+++ composeApp/src/iosMain/kotlin/com/leoapps/eggy/common/vibrator/VibrationManagerIosImpl.kt
@@ -4,12 +4,7 @@ import com.leoapps.eggy.common.vibration.domain.VibrationManager
 import platform.UIKit.UIImpactFeedbackGenerator
 import platform.UIKit.UIImpactFeedbackStyle
 import platform.UIKit.UINotificationFeedbackGenerator
-import platform.UIKit.UIUserNotificationType
-import platform.darwin.DISPATCH_TIME_NOW
-import platform.darwin.dispatch_after
-import platform.darwin.dispatch_get_main_queue
-import platform.darwin.dispatch_queue_t
-import platform.darwin.dispatch_time
+import platform.UIKit.UINotificationFeedbackType
 
 class VibrationManagerIosImpl : VibrationManager {
 
@@ -24,28 +19,7 @@ class VibrationManagerIosImpl : VibrationManager {
     }
 
     override fun vibratePattern(pattern: LongArray) {
-        // iOS doesn't natively support custom vibration patterns.
-        // Simulate a pattern using delays and the `kSystemSoundID_Vibrate` sound.
-        simulateVibrationPattern(pattern)
-    }
-
-    private fun simulateVibrationPattern(pattern: LongArray) {
-//        val queue: dispatch_queue_t = dispatch_get_main_queue()
-//        pattern.forEachIndexed { index, duration ->
-//            dispatch_after(
-//                dispatch_time(DISPATCH_TIME_NOW, duration * 1_000_000),
-//                queue
-//            ) {
-//                if (index % 2 == 0) {
-//                    notificationFeedbackGenerator.notificationOccurred(
-//                        when (index % 3) {
-//                            0 -> UIUserNotificationType.UIUserUIUserNotificationTypeNone
-//                            1 -> UIUserNotificationType.UIUserNotificationTypeAlert
-//                            else -> UIUserNotificationType.UIUserNotificationTypeAll;
-//                        }
-//                    )
-//                }
-//            }
-//        }
+        notificationFeedbackGenerator.prepare()
+        notificationFeedbackGenerator.notificationOccurred(UINotificationFeedbackType.UINotificationFeedbackTypeError)
     }
 }
